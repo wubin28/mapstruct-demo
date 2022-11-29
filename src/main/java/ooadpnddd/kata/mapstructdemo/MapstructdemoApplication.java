cdaca64845aec97678865e4329db048868c05f8f
Qian <wubin28@outlook.com>
2022-11-29 11:32:42 +0800

    chore: create a blank project using start.spring.io

---
+++ src/main/java/ooadpnddd/kata/mapstructdemo/MapstructdemoApplication.java
@@ -0,0 +1,13 @@
+package ooadpnddd.kata.mapstructdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ooadpnddd/kata/mapstructdemo/MapstructdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ooadpnddd.kata.mapstructdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
